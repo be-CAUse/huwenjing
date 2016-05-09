42a82577cda506e995f4d5d708490544cc4f595b
码农教育24 <manongjiaoyu24@manongjiaoyu24demac-mini.local>
2016-05-09 14:41:29 +0800

    Initial Commit

---
+++ in/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  in
+//
+//  Created by 码农教育24 on 16/5/9.
+//  Copyright © 2016年 码农教育24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ in/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  in
+//
+//  Created by 码农教育24 on 16/5/9.
+//  Copyright © 2016年 码农教育24. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
